f5266fdd90220af24732a95e18095787a9c91057
CEaussadawut <67015165@kmitl.ac.th>
2025-09-14 20:46:35 +0700

    v05

--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    whatsyourname.py                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: aussapha <aussapha@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2025/09/14 20:44:02 by aussapha          #+#    #+#              #
+#    Updated: 2025/09/14 20:44:02 by aussapha         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 first_name = input("Hey, what's your first name? : ").strip()
 last_name = input("And your last name? : ").strip()
 whole_name = first_name + ' ' + last_name
